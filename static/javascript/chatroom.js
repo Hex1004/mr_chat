f79c71ab6a05d049e2e1aabca24ce4c98431fae2
Hristo Kirilov <hristo.kirilov07@gmail.com>
2024-12-02 23:55:23 +0200

    code

--- static/javascript/chatroom.js
+++ static/javascript/chatroom.js
@@ -1,4 +1,4 @@
-   document.getElementById('show-search-button').addEventListener('click', function () {
+ document.getElementById('show-search-button').addEventListener('click', function () {
     document.getElementById('search-container').style.display = 'flex';
 });
 
